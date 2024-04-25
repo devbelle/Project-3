7e20293c452f97340ccb59d9d5e69d736129348c
amaher <amaher112@su.edu>
2024-04-25 19:21:28 -0400

    created models for User and Trip

---
+++ Adventure Awaits/server/models/Trip.js
@@ -0,0 +1,36 @@
+// Destination
+// start date
+// end date
+// notes
+
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const tripSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  destination: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+});
+
+const Trip = mongoose.model("Trip", tripSchema);
+
+module.exports = Trip;
