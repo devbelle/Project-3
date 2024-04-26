1799a48a6bda3a9785c4e303832483284e5685c5
Lea Arcidiacono <live2run_1980@yahoo.com>
2024-04-26 00:00:08 -0400

    login styling

--- Adventure Awaits/client/src/components/Home.jsx
+++ Adventure Awaits/client/src/components/Home.jsx
@@ -1,25 +1,25 @@
 
 import LoginPage from './pages/LoginPage';
-import Trips from './pages/Trips';
+// import Trips from './pages/Trips';
 import Restaurants from './pages/Restaurants';
 import Hotels from './pages//Hotels';
-import PackingPage from './pages/PackingPage';
+// import PackingPage from './pages/PackingPage';
 
 import PropTypes from 'prop-types';
 
 function Home({ section }) {
   if (section === 'Login') {
     return <LoginPage />;
-  } else if (section === 'Trips') {
-    return <Trips />;
+  // } else if (section === 'Trips') {
+  //   return <Trips />;
   } else if (section === 'Restaurants') {
     return <Restaurants/>;
   } else if (section === 'Hotels') {
 return <Hotels/>;
 }
-else if (section === 'Packing') {
-  return <PackingPage/>;
-}
+// else if (section === 'Packing') {
+//   return <PackingPage/>;
+// }
 }
 
 Home.propTypes = {
--- Adventure Awaits/client/src/main.jsx
+++ Adventure Awaits/client/src/main.jsx
@@ -3,20 +3,20 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import LoginPage from './components/pages/LoginPage.jsx'
-import Hotels from './components/pages/Hotels.jsx'
-import Restaurants from './components/pages/Restaurants.jsx'
-import Trip from './components/pages/Trip.jsx'
-import PackingPage from './components/pages/PackingPage.jsx'
+// import Hotels from './components/pages/Hotels.jsx'
+// import Restaurants from './components/pages/Restaurants.jsx'
+// import Trip from './components/pages/Trip.jsx'
+// import PackingPage from './components/pages/PackingPage.jsx'
 
 const router = createBrowserRouter([
   { path: '/', 
   element: <App />, 
   children: [
     { index: true, element: <LoginPage /> },
-     { path: 'hotels', element: <Hotels/> }, 
-     { path: 'restaurants', element: <Restaurants/> },
-     { path: 'trip', element: <Trip/>},
-     { path: 'packing', element: <PackingPage/>}
+    //  { path: 'hotels', element: <Hotels/> }, 
+    //  { path: 'restaurants', element: <Restaurants/> },
+    //  { path: 'trip', element: <Trip/>},
+    //  { path: 'packing', element: <PackingPage/>}
 
     ]
   }
