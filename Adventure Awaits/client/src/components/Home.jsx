ea88a786a3ec33a2e0931e354ffd09592d04c0ea
Lea Arcidiacono <live2run_1980@yahoo.com>
2024-04-25 21:02:16 -0400

    index app main home jsx

--- Adventure Awaits/client/src/components/Home.jsx
+++ Adventure Awaits/client/src/components/Home.jsx
@@ -1,5 +1,29 @@
 
+import LoginPage from './pages/LoginPage';
+import Trips from './pages/Trips';
+import Restaurants from './pages/Restaurants';
+import Hotels from './pages//Hotels';
 
+import PropTypes from 'prop-types';
+
+function Home({ section }) {
+  if (section === 'Login') {
+    return <LoginPage />;
+  } else if (section === 'Trips') {
+    return <Trips />;
+  } else if (section === 'Restaurants') {
+    return <Restaurants/>;
+  } else if (section === 'Hotels') {
+return <Hotels/>;
+}
+}
+
+Home.propTypes = {
+  section: PropTypes.string.isRequired,
+};
+
+
+export default Home;
 
 
 
--- Adventure Awaits/client/src/components/pages/index.jsx
+++ Adventure Awaits/client/src/components/pages/index.jsx
@@ -1,11 +1,9 @@
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 
-function Page (  {currentPage})  {
-    const page = currentPage.substring(1)
-    console.log(page);
-    return (
-    <div> <Outlet/> </div>
-    )
+function Page() {
+  return (
+    <div><Outlet/></div>
+  );
 }
 
-export default Page ;
+export default Page;
