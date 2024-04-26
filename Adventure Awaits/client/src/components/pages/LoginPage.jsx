e8d028d3f26e801c7637c77cb9f3381b33d3f023
Lea Arcidiacono <live2run_1980@yahoo.com>
2024-04-25 23:45:14 -0400

    stying login page

--- Adventure Awaits/client/src/components/pages/LoginPage.jsx
+++ Adventure Awaits/client/src/components/pages/LoginPage.jsx
@@ -6,21 +6,69 @@ import { useState } from 'react';
 const Box = styled.div`
 display: flex;
 justify-content: space-between;
-  width: 600px;
-  padding: 20px;
+  width: 90%;
+  max-width: 430px;
+  padding: 10px;
   background-color: white;
-  border-radius: 8px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  aligh-items: center;
+  position: absolute;
+border: 3px solid black;
+margin-bottom: 250px;
 `;
 
 const Section = styled.div`
   width: 50%;
+  background-color: #FFAD73;
+  border: 1px solid black;
+`;
+
+const Container = styled.div`
+  background-color: #ADD8E6;  
+  width: 100%;
+  min-height: 100vh;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  `;
+const Button = styled.button`
+width: 100px;
+display: block;
+border: 1px solid black;
+padding: 5px;
+margin-top: 10px;
+`;
+const Input = styled.input`
+padding: 5px;
+margin: 5px;
+`;
+const ImageBox = styled.div`
+width: 100px;
+height: 90px;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: auto; 
+margin-bottom: 90px;
 `;
 
 const LoginPage = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [signupName, setSignupName] = useState('');
+  const [signupEmail, setSignupEmail] = useState('');
+  const [signupPassword, setSignupPassword] = useState('');
+  const [loginName, setLoginName] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+
 
     const handleSignUp =  (event) => {
         event.preventDefault();
@@ -32,50 +80,55 @@ const handleLogin = (event) => {
 };
 
 return (
+  <Container>
     <Box>
       <Section>
-        <h2>Create a New Account</h2>
-        <form onSubmit={handleSignUp}>
-          <input
+      <h2 style={{ color: '#333', textAlign: 'center', fontSize:' 20px' }}>Create a New Account</h2>       
+       <Form onSubmit={handleSignUp}>
+       <Input
             type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={signupName}
+            onChange={(e) => setSignupName(e.target.value)}
             placeholder="Name"
           />
-          <input
+          <Input
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={signupEmail}
+            onChange={(e) => setSignupEmail(e.target.value)}
             placeholder="Email"
           />
-          <input
+          <Input
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={signupPassword}
+            onChange={(e) => setSignupPassword(e.target.value)}
             placeholder="Password"
           />
-          <button type="submit">Sign up</button>
-        </form>
+          <Button type="submit">Sign up</Button>
+        </Form>
       </Section>
       <Section>
-        <h2>Already have an account?</h2>
-        <form onSubmit={handleLogin}>
-          <input
+      <h2 style={{ color: '#333', textAlign: 'center' , fontSize: '20px'}}>Already Have an Account? Log In.</h2>       
+       <Form onSubmit={handleLogin}>
+          <Input
             type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={loginName}
+            onChange={(e) => setLoginName(e.target.value)}
             placeholder="Name"
           />
-          <input
+          <Input
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={loginPassword}
+            onChange={(e) => setLoginPassword(e.target.value)}
             placeholder="Password"
           />
-          <button type="submit">Log in</button>
-        </form>
+          <Button type="submit">Log in</Button>
+        </Form>
       </Section>
     </Box>
+    <ImageBox>
+      <img src="/images/travel.jpg" alt="travel quote" style={{ width: '200%', height: '250%' }} />
+    </ImageBox>
+    </Container>
   );
 };
 
