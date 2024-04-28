f4f3057ded58bfe943267c1d0ce2cea7ab4fa852
Devin <dbelle722@gmail.com>
2024-04-28 16:15:45 -0400

    Form edits and styling

--- Adventure Awaits/client/src/pages/PackingPage.jsx
+++ Adventure Awaits/client/src/pages/PackingPage.jsx
@@ -3,6 +3,41 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import Auth from '../utils/auth';
 
+const Box = styled.div`
+display: flex;
+justify-content: space-between;
+width: 90%;
+max-width: 430px;
+height: 220px;
+padding: 10px;
+background-color: white;
+border-radius: 10px;
+align-items: center;
+position: absolute;
+border: 3px solid black;
+margin-bottom: 250px;
+`
+
+const Section = styled.div`
+width: 50%;
+height: 100%;
+background-color: #ffad73;
+border: 1px solid black;
+`
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Button = styled.button`
+  width: 100px;
+  display: block;
+  border: 1px solid black;
+  padding: 5px;
+  margin-top: 10px;
+`;
+
 
 //from Devin's React Portfolio
 const Trip = () => {
@@ -19,24 +54,24 @@ const Trip = () => {
 
 
     return (
-        <div>
-            <div>
+        <Box>
+            <Section>
                 <h2>What should you bring?</h2>
-            </div>
-            <form id="contact-form" onSubmit={/* submit button */}>
+            </Section>
+            <Form id="contact-form" onSubmit={/* submit button */}>
         
-                <div className="field">
+                <Section className="field">
                     <label className="label" htmlFor="message">Packing...</label>
                     <textarea className="textarea" name="message" id="text" rows="5" defaultValue={/* message function for user entry of packing*/} onBlur={/* handlefrom function*/} />
-                </div>
+                </Section>
                     {/* error message function*/ && (
-                <div>
+                <Section>
                     <p className="is-danger">{/* Error message function*/}</p>
-                </div>
+                </Section>
                 )}
-                    <button className="button is-medium is-primary is-fullwidth" data-testid="button" type="submit">Submit</button>
-            </form>
-        </div>
+                    <Button className="button is-medium is-primary is-fullwidth" data-testid="button" type="submit">Submit</Button>
+            </Form>
+        </Box>
 
     )
 
