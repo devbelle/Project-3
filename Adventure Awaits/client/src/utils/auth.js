99e7a6533cee9ec135812374fda6f8515c1f74e5
Lea Arcidiacono <live2run_1980@yahoo.com>
2024-04-27 23:17:57 -0400

    bcrypt added auth added login fixes

--- Adventure Awaits/client/src/utils/auth.js
+++ Adventure Awaits/client/src/utils/auth.js
@@ -1,36 +1,37 @@
-// // use this to decode a token and get the user's information out of it
-// import decode from 'jwt-decode';
+// use this to decode a token and get the user's information out of it
+import jwtDecode from 'jwt-decode';
 
-// // create a new class to instantiate for a user
-// class AuthService {
-//   // get user data from JSON web token by decoding it
-//   getProfile() {
-//     return decode(this.getToken());
-//   }
+// create a new class to instantiate for a user
+class AuthService {
+  getProfile() {
+    const token = this.getToken();
+    return jwtDecode(token);
+  }
 
-//   // return `true` or `false` if token exists (does not verify if it's expired yet)
-//   loggedIn() {
-//     const token = this.getToken();
-//     return token ? true : false;
-//   }
 
-//   getToken() {
-//     // Retrieves the user token from localStorage
-//     return localStorage.getItem('id_token');
-//   }
+  // return `true` or `false` if token exists (does not verify if it's expired yet)
+  loggedIn() {
+    const token = this.getToken();
+    return token ? true : false;
+  }
 
-//   login(idToken) {
-//     // Saves user token to localStorage and reloads the application for logged in status to take effect
-//     localStorage.setItem('id_token', idToken);
-//     window.location.assign('/');
-//   }
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
+  }
 
-//   logout() {
-//     // Clear user token and profile data from localStorage
-//     localStorage.removeItem('id_token');
-//     // this will reload the page and reset the state of the application
-//     window.location.reload();
-//   }
-// }
+  login(idToken) {
+    // Saves user token to localStorage and reloads the application for logged in status to take effect
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
 
-// export default new AuthService();
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // this will reload the page and reset the state of the application
+    window.location.reload();
+  }
+}
+
+export default new AuthService();
