5adc78637c85bc4a330ecd2cfcf07d87dbec0f44
Lea Arcidiacono <live2run_1980@yahoo.com>
2024-04-28 22:14:37 -0400

    header, navlinks, login styling update, index update

--- Adventure Awaits/client/src/main.jsx
+++ Adventure Awaits/client/src/main.jsx
@@ -3,24 +3,23 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import LoginPage from './pages/LoginPage.jsx'
-// import Hotels from './pages/HotelsPage.jsx'
-// import Restaurants from './pages/RestaurantsPage.jsx'
-// import TripsPage from './pages/TripsPage.jsx'
-// import PackingPage from './pages/PackingPage.jsx'
+import Hotels from './pages/HotelsPage.jsx'
+import Restaurants from './pages/RestaurantsPage.jsx'
+import TripsPage from './pages/TripsPage.jsx'
+import PackingPage from './pages/PackingPage.jsx'
 
 const router = createBrowserRouter([
   { path: '/', 
   element: <App />, 
   children: [
     { index: true, element: <LoginPage /> },
-    //  { path: 'hotels', element: <Hotels/> }, 
-    //  { path: 'restaurants', element: <Restaurants/> },
-    //  { path: 'trips', element: <TripsPage/>},
-    //  { path: 'packing', element: <PackingPage/>}
-
-    ]
+    { path: 'hotels', element: <Hotels/> }, 
+    { path: 'restaurants', element: <Restaurants/> },
+    { path: 'trips', element: <TripsPage/>},
+    { path: 'packing', element: <PackingPage/>}
+  ]
   }
-])  
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
