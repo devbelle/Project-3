421b9f827b11c091b66151856ab971fa0a9f6694
Lea Arcidiacono <live2run_1980@yahoo.com>
2024-04-29 20:02:51 -0400

    login page loads

--- Adventure Awaits/client/src/main.jsx
+++ Adventure Awaits/client/src/main.jsx
@@ -3,22 +3,22 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import LoginPage from './pages/LoginPage.jsx'
-import Hotels from './pages/HotelsPage.jsx'
-import Restaurants from './pages/RestaurantsPage.jsx'
-import TripsPage from './pages/TripsPage.jsx'
-import PackingPage from './pages/PackingPage.jsx'
-import EditTripPage from './pages/EditTripPage.jsx'
+// import Hotels from './pages/HotelsPage.jsx'
+// import Restaurants from './pages/RestaurantsPage.jsx'
+// import TripsPage from './pages/TripsPage.jsx'
+// import PackingPage from './pages/PackingPage.jsx'
+// import EditTripPage from './pages/EditTripPage.jsx'
 
 const router = createBrowserRouter([
   { path: '/', 
   element: <App />, 
   children: [
     { index: true, element: <LoginPage /> },
-     { path: 'hotels', element: <Hotels/> }, 
-     { path: 'restaurants', element: <Restaurants/> },
-     { path: 'trips', element: <TripsPage/>},
-     { path: 'packing', element: <PackingPage/>},
-     { path: 'EditTripPage', element: <EditTripPage/>}
+    //  { path: 'hotels', element: <Hotels/> }, 
+    //  { path: 'restaurants', element: <Restaurants/> },
+    //  { path: 'trips', element: <TripsPage/>},
+    //  { path: 'packing', element: <PackingPage/>},
+    //  { path: 'EditTripPage', element: <EditTripPage/>}
 
     ]
   }
