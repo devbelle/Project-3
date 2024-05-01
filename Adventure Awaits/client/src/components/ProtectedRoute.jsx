232fd11745d33d9cf908b923e0364a7a2a2604bd
Lea Arcidiacono <live2run_1980@yahoo.com>
2024-04-30 20:12:35 -0400

    props added

--- Adventure Awaits/client/src/components/ProtectedRoute.jsx
+++ Adventure Awaits/client/src/components/ProtectedRoute.jsx
@@ -1,5 +1,7 @@
 import { Route, Navigate, useLocation } from 'react-router-dom';
 
+import PropTypes from 'prop-types';
+
 function ProtectedRoute({ element, ...rest }) {
   const token = localStorage.getItem('token');
   const isAuthenticated = token != null;
@@ -11,5 +13,11 @@ function ProtectedRoute({ element, ...rest }) {
     <Navigate to="/" state={{ from: location }} />
   );
 }
+
+ProtectedRoute.propTypes = {
+  element: PropTypes.element.isRequired,
+};
+
+
 
 export default ProtectedRoute;
