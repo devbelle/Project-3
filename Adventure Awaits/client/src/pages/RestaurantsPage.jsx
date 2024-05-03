7bc6d5b8f7b6ce1f34f3eff405dbda72829b7174
amaher <amaher112@su.edu>
2024-05-03 17:44:12 -0400

    added functionality to front end restaurant api call

--- Adventure Awaits/client/src/pages/RestaurantsPage.jsx
+++ Adventure Awaits/client/src/pages/RestaurantsPage.jsx
@@ -1,61 +1,53 @@
-import HeaderPages from '../components/HeaderPages';
+import HeaderPages from "../components/HeaderPages";
 import styled from "styled-components";
-import {useState} from 'react';
-import { useQuery } from "@apollo/client";
+import { useState } from "react";
+import { useLazyQuery } from "@apollo/client";
 import { GET_RESTAURANTS } from "../utils/queries";
-// import { getRestaurants } from "../utils/queries"; 
 
-
-  const Section = styled.div`
-    width: 50%;
-    height: 100%;
-    background-color: #ffad73;
-    border: 1px solid black;
-  `;
-  const Form = styled.form`
+const Section = styled.div`
+  width: 50%;
+  height: 100%;
+  background-color: #ffad73;
+  border: 1px solid black;
+`;
+const Form = styled.form`
     display: flex
     flex-direction: column;
     align-items: center;
     justify-content: center;
   `;
-  const Button = styled.button`
-    width: 100px;
-    display: block;
-    border: 1px solid black;
-    padding: 5px;
-    margin-top: 10px;
-  `;
-  const Input = styled.input`
-    padding: 5px;
-    margin: 5px;
-  `;
+const Button = styled.button`
+  width: 100px;
+  display: block;
+  border: 1px solid black;
+  padding: 5px;
+  margin-top: 10px;
+`;
+const Input = styled.input`
+  padding: 5px;
+  margin: 5px;
+`;
 
-  const RestaurantsPage = () => {
-    const [restaurantCitySearch, setRestaurantCitySearch] = useState('');
-    const { loading, error, data } = useQuery(GET_RESTAURANTS);
+const RestaurantsPage = () => {
+  const [restaurantCitySearch, setRestaurantCitySearch] = useState("");
+  const [getRestaurants, { loading, error, data, called }] = useLazyQuery(
+    GET_RESTAURANTS,
+    {
+      variables: { city: restaurantCitySearch },
+    }
+  );
+  console.log(restaurantCitySearch);
 
   // restaurant stuff
-  // const handleFormSubmit = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     const { data } = await getRestaurants({
-  //       variables: { city: "Barcelona" },
-  //     });
-  //     console.log(data.getRestaurants);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-    if (loading) return;
-    if (error) {
-      console.error(error);
-      return;
-    }
-    console.log(data.getRestaurants);
+
+    await getRestaurants();
   };
+  if (loading && called) return <div>Loading...</div>;
+  if (error) return `Error! ${error.message}`;
+
+  const restaurants = data?.getRestaurants || [];
 
   return (
     <div>
@@ -68,7 +60,7 @@ import { GET_RESTAURANTS } from "../utils/queries";
         imgSrc="/images/globe.jpg"
       />
 
-      <Form id="contact-form" onSubmit={handleFormSubmit}>
+      <Form onSubmit={handleFormSubmit}>
         <Section className="field">
           <label className="label" htmlFor="city">
             Search for a City
@@ -79,7 +71,7 @@ import { GET_RESTAURANTS } from "../utils/queries";
             name="restaurant"
             placeholder="Search for a City..."
             value={restaurantCitySearch}
-            onChange={e => setRestaurantCitySearch(e.target.value)}
+            onChange={(e) => setRestaurantCitySearch(e.target.value)}
           />
         </Section>
 
@@ -91,8 +83,11 @@ import { GET_RESTAURANTS } from "../utils/queries";
           Search
         </Button>
       </Form>
+
+      {restaurants.length > 0
+        ? restaurants.map((restaurant) => <div> {restaurant.name}</div>)
+        : null}
     </div>
   );
 };
 export default RestaurantsPage;
-
