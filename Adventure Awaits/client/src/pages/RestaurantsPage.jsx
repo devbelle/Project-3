b8bf22b0b384398d6112aa479aa0bc6cf465c959
amaher <amaher112@su.edu>
2024-05-03 20:45:12 -0400

    page fix

--- Adventure Awaits/client/src/pages/RestaurantsPage.jsx
+++ Adventure Awaits/client/src/pages/RestaurantsPage.jsx
@@ -4,18 +4,20 @@ import { useState } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { GET_RESTAURANTS } from "../utils/queries";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Card, ListGroup} from "react-bootstrap";
+import { Card, ListGroup } from "react-bootstrap";
 
 const Section = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  height: 70vh;
   min-height: 40vh;
   width: 100%;
-  margin-top: -120px;
   @media (max-width: 768px) {
-    width: 70%; // Decrease width on smaller screens
+    height: 80vh; // Increase height on smaller screens
+    min-height: 90vh; // Increase minimum height on smaller screens
+    width: 100%; // Increase width on smaller screens
   }
 `;
 
@@ -23,56 +25,77 @@ const ParentDiv = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  min-height: 80vh;
+  min-height: 40vh;
   width: 100vw;
+  @media (max-width: 768px) {
+    min-height: 60vh; // Increase minimum height on smaller screens
+  }
 `;
 
 const Form = styled.form`
+margin-top: -80px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 300px;
-  height: 260px;
+  min-height: 50%; 
+  width: 25%;
   border: 2px solid black;
-  margin-top: 10px;
   border-radius: 10px;
-  background-image: url("images/burger.png");
-  background-repeat: no-repeat;
-  background-size: cover%;
-  filter: brightness(90%);
+  background-image: url('/images/burger.png');
+  background-size: cover;
+  @media (max-width: 768px) {
+    width: 60%; // Increase width on smaller screens
+    min-height: 40%; // Increase height on smaller screens
+  }
 `;
-
 const Button = styled.button`
-  width: 100px;
   display: block;
   border: 2px solid black;
   padding: 8px;
   margin-top: 8px;
   margin-bottom: 5px;
-  width: 45%;
+  width: 45%; 
   text-align: center;
   transition: all 0.3s ease; // Add transition
   &:hover {
     transform: scale(1.02); // Increase size on hover
   }
-  background-color: #add8e6;
+  background-color: #ADD8E6;
   @media (max-width: 768px) {
-    width: 50%; // Increase width on smaller screens
-    height: 60px; // Increase height on smaller screens
+    width: 40%; // Increase width on smaller screens
   }
 
-  @media (max-width: 735px) {
-    width: 40%; // Increase width on even smaller screens
-    height: 40px; // Increase height on even smaller screens
+  @media (max-width: 480px) {
+    width: 80%; // Further increase width on very small screens
   }
 `;
+
 const Input = styled.input`
   padding: 5px;
-  margin: 5px;
+  width: 90%; 
+  margin-top: 2px;
+  text-align: center;
+  transition: all 0.3s ease; // Add transition
+  &:hover {
+    transform: scale(1.02); // Increase size on hover
+  }
+  @media (max-width: 480px) {
+    width: 60; // Further increase width on very small screens
+  }
 `;
 
-
+const Label = styled.label`
+display: block;
+  width: 100%;
+  text-align: center;
+  font-weight: bold;
+margin-bottom: 10px;
+font-size: 28px;
+color: white;
+margin-top: -50px;
+padding-bottom: 2px;
+  `;
 
 const RestaurantsPage = () => {
   const [restaurantCitySearch, setRestaurantCitySearch] = useState("");
@@ -131,15 +154,20 @@ const RestaurantsPage = () => {
 
       {restaurants.length > 0
         ? restaurants.map((restaurant) => (
-            <Card key={restaurant.locationId} style={{ width: '18rem' }} >
-              <Card.Header as="h4" className="text-center">{restaurant.name}</Card.Header>
+            <Card key={restaurant.locationId} style={{ width: "18rem" }}>
+              <Card.Header as="h4" className="text-center">
+                {restaurant.name}
+              </Card.Header>
               <Card.Body>
-              <Card.Img src={restaurant.squareImgUrl} />
+                <Card.Img src={restaurant.squareImgUrl} />
                 <ListGroup>
-                  <ListGroup.Item>Average Rating: {restaurant.averageRating}</ListGroup.Item>
+                  <ListGroup.Item>
+                    Average Rating: {restaurant.averageRating}
+                  </ListGroup.Item>
                   <ListGroup.Item>Price: {restaurant.priceTag}</ListGroup.Item>
-                  <ListGroup.Item>{restaurant.currentOpenStatusText}</ListGroup.Item>
-                  
+                  <ListGroup.Item>
+                    {restaurant.currentOpenStatusText}
+                  </ListGroup.Item>
                 </ListGroup>
               </Card.Body>
             </Card>
